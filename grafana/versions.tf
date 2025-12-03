44c6f543b739e98149743a81c6222d8172be5e8a
Jakub Jindra <jakub.jindra@cookielab.io>
2025-12-03 14:07:44 +0100

    chore(deps): change constraint for hashicorp/helm from ~>3.0 to >=3.0.0

--- grafana/versions.tf
+++ grafana/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 3.0"
+      version = ">= 3.0.0"
     }
   }
   required_version = ">= 1.5, < 2.0"
--- tempo/modules/shared/versions.tf
+++ tempo/modules/shared/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 3.0"
+      version = ">= 3.0.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"

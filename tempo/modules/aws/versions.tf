5656a88b80189a58da14f471d6a24d15e4e99ed8
Jindrich Skupa <jindrich.skupa@gmail.com>
2026-04-01 16:55:31 +0200

    chore(deps): relax aws provider version

--- tempo/modules/aws/versions.tf
+++ tempo/modules/aws/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.94"
+      version = ">= 5.94"
     }
   }
   required_version = ">= 1.9, < 2.0"
